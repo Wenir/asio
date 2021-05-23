e8a79b71baf8a443a863f448e5e81e4795cdaec7
Christopher Kohlhoff <chris@kohlhoff.com>
2021-05-23 20:25:28 +1000

    Add cancellation_slot support to async_write_at.

--- asio/include/asio/impl/write_at.hpp
+++ asio/include/asio/impl/write_at.hpp
@@ -20,6 +20,7 @@
 #include "asio/buffer.hpp"
 #include "asio/completion_condition.hpp"
 #include "asio/detail/array_fwd.hpp"
+#include "asio/detail/base_from_cancellation_state.hpp"
 #include "asio/detail/base_from_completion_cond.hpp"
 #include "asio/detail/bind_handler.hpp"
 #include "asio/detail/consuming_buffers.hpp"
@@ -161,14 +162,15 @@ namespace detail
       typename ConstBufferSequence, typename ConstBufferIterator,
       typename CompletionCondition, typename WriteHandler>
   class write_at_op
-    : detail::base_from_completion_cond<CompletionCondition>
+    : public base_from_cancellation_state<WriteHandler>,
+      base_from_completion_cond<CompletionCondition>
   {
   public:
     write_at_op(AsyncRandomAccessWriteDevice& device,
         uint64_t offset, const ConstBufferSequence& buffers,
         CompletionCondition& completion_condition, WriteHandler& handler)
-      : detail::base_from_completion_cond<
-          CompletionCondition>(completion_condition),
+      : base_from_cancellation_state<WriteHandler>(handler),
+        base_from_completion_cond<CompletionCondition>(completion_condition),
         device_(device),
         offset_(offset),
         buffers_(buffers),
@@ -179,7 +181,8 @@ namespace detail
 
 #if defined(ASIO_HAS_MOVE)
     write_at_op(const write_at_op& other)
-      : detail::base_from_completion_cond<CompletionCondition>(other),
+      : base_from_cancellation_state<WriteHandler>(other),
+        base_from_completion_cond<CompletionCondition>(other),
         device_(other.device_),
         offset_(other.offset_),
         buffers_(other.buffers_),
@@ -189,8 +192,11 @@ namespace detail
     }
 
     write_at_op(write_at_op&& other)
-      : detail::base_from_completion_cond<CompletionCondition>(
-          ASIO_MOVE_CAST(detail::base_from_completion_cond<
+      : base_from_cancellation_state<WriteHandler>(
+          ASIO_MOVE_CAST(base_from_cancellation_state<
+            WriteHandler>)(other)),
+        base_from_completion_cond<CompletionCondition>(
+          ASIO_MOVE_CAST(base_from_completion_cond<
             CompletionCondition>)(other)),
         device_(other.device_),
         offset_(other.offset_),
@@ -201,7 +207,7 @@ namespace detail
     }
 #endif // defined(ASIO_HAS_MOVE)
 
-    void operator()(const asio::error_code& ec,
+    void operator()(asio::error_code ec,
         std::size_t bytes_transferred, int start = 0)
     {
       std::size_t max_size;
@@ -209,7 +215,7 @@ namespace detail
       {
         case 1:
         max_size = this->check_for_completion(ec, buffers_.total_consumed());
-        do
+        for (;;)
         {
           {
             ASIO_HANDLER_LOCATION((__FILE__, __LINE__, "async_write_at"));
@@ -222,9 +228,17 @@ namespace detail
           if ((!ec && bytes_transferred == 0) || buffers_.empty())
             break;
           max_size = this->check_for_completion(ec, buffers_.total_consumed());
-        } while (max_size > 0);
+          if (max_size == 0)
+            break;
+          if (this->cancelled())
+          {
+            ec = asio::error::operation_aborted;
+            break;
+          }
+        }
 
-        handler_(ec, buffers_.total_consumed());
+        handler_(static_cast<const asio::error_code&>(ec),
+            static_cast<const std::size_t&>(buffers_.total_consumed()));
       }
     }
 
@@ -616,6 +630,25 @@ struct associated_executor<
       const Executor1& ex = Executor1()) ASIO_NOEXCEPT
   {
     return associated_executor<WriteHandler, Executor1>::get(h.handler_, ex);
+  }
+};
+
+template <typename Executor, typename WriteHandler, typename CancellationSlot>
+struct associated_cancellation_slot<
+    detail::write_at_streambuf_op<Executor, WriteHandler>,
+    CancellationSlot>
+  : detail::associated_cancellation_slot_forwarding_base<
+      WriteHandler, CancellationSlot>
+{
+  typedef typename associated_cancellation_slot<
+      WriteHandler, CancellationSlot>::type type;
+
+  static type get(
+      const detail::write_at_streambuf_op<Executor, WriteHandler>& h,
+      const CancellationSlot& s = CancellationSlot()) ASIO_NOEXCEPT
+  {
+    return associated_cancellation_slot<WriteHandler,
+        CancellationSlot>::get(h.handler_, s);
   }
 };
 
